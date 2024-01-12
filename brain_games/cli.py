5a594386d832612f6027cc111b3cab28f8719ce5
anton <mr.fomkin@bk.ru>
2024-01-13 02:24:07 +0300

    3 step

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt
+
+def welcome_user():
+	user = prompt.string('May I have your name? ')
+	return f'Hello, {user}!'
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
-print('Welcome to the Brain Games!')
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
 
 def main():
-	pass
+	print('Welcome to the Brain Games!')
+	print(welcome_user())
+
 
 if __name__ == '__main__':
 	main()
