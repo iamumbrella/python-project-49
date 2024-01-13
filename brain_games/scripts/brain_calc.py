e363db38d365941858fdcf6d01ab10c3afa0e96e
anton <mr.fomkin@bk.ru>
2024-01-13 22:21:51 +0300

    6 step

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env/python
 
-from brain_games.games import even_game
+from brain_games.games import calc_game
 from brain_games.logic import launch
 
 
 def main():
-    launch(even_game)
+    launch(calc_game)
 
 
 if __name__ == '__main__':
     main()
+
 
